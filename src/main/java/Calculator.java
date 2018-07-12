0665faa7d0a48baed47fd9112af87d20ed73e523
“sachinidezoysa” <bawanthisachini1995@gmail.com>
2018-07-12 13:38:24 +0530

    mocked calculator class, added multiplyCons() method and multiplyStatic() method in CalculatorImpl class

--- src/main/java/Calculator.java
+++
@@ -1,7 +0,0 @@
-public interface Calculator {
-
-    public int add(int x, int y);
-    public int multiply(int x, int y);
-    public int minus(int x, int y);
-    public int divide(int x, int y);
-}
---
+++ src/main/java/Constants.java
@@ -0,0 +1,4 @@
+public class Constants {
+
+    private int g = 10;
+}
